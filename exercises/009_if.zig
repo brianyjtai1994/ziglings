e721e7c7c5ae949632a61a8f1aac2b503631f5f6
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-02-07 20:46:10 +0800

    Ans. 009_if.zig

--- exercises/009_if.zig
+++ exercises/009_if.zig
@@ -21,13 +21,13 @@
 const std = @import("std");
 
 pub fn main() void {
-    const foo = 1;
+    const foo: bool = true;
 
     // Please fix this condition:
     if (foo) {
         // We want our program to print this message!
-        std.debug.print("Foo is 1!\n", .{});
+        std.debug.print("Foo is true!\n", .{});
     } else {
-        std.debug.print("Foo is not 1!\n", .{});
+        std.debug.print("Foo is not true!\n", .{});
     }
 }
